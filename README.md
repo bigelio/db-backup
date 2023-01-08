8b9af6f10b9fb64d2989ff0c663c3731597416ea
Gelio BIZIMANA <70667354+bigelio@users.noreply.github.com>
2023-01-08 15:14:12 +0200

    chore: update README file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # db-backup
+
 Mongodb backup script with nodejs.
+
+This cron job backup mongodb database every mid-night.
+
+## prerequisites
+
+- nodejs/npm
+
+## Scripts
+
+```
+node index.js
+```
--- index.js
+++ index.js
@@ -2,30 +2,41 @@ const { spawn } = require("child-process");
 const path = require("path");
 
 const cron = require("node-cron");
+/**
+ * requiring all neccessary variables
+ */
 
-const DB_NAME = `db`;
+const DB_NAME = process.env.DB_NAME;
 const ARCHIVE_PATH = path.join(__dirname, "backup", `${DB_NAME}.gzip`);
-
-// run cron job “At every mid-night.”
+/**
+ * run cron job “At every mid-night.”
+ */
 
 cron.schedule("0 0 * * * *", () => backUpMongodb());
 
-// db backup function
+/**
+ *  db backup function
+ */
+
 const backUpMongodb = () => {
   const child = spawn("mongodump", [
     `--db=${DB_NAME}`,
     `--archive=${ARCHIVE_PATH}`,
     `gzip`,
   ]);
+
+  // standard out
   child.stdout.on("data", (data) => {
     console.log("stdout:\n", data);
   });
+  // standard err
   child.stderr.on("data", (data) => {
     console.log("stderr:\n", Buffer.from(data).toString());
   });
   child.stdout.on("error", (error) => {
     console.log("error:\n", error);
   });
+  // Handle node process error
   child.on("exit", (code, signal) => {
     if (code) console.log("process exit with code ", code);
     else if (signal) console.log("process killed with signal ", signal);
